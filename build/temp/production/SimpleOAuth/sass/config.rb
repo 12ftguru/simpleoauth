7b9bfad815e87342b3e7eb0a217015e8ef1a1867
Bryan P Johnson <bryan@12ftguru.com>
2015-05-21 13:52:19 -0400

    Unignored build dir.
    To try to fix sencha cmd issues..
    
    Signed-off-by: Bryan P Johnson <bryan@12ftguru.com>

---
+++ build/temp/production/SimpleOAuth/sass/config.rb
@@ -0,0 +1,3 @@
+require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/bryan/Projects/simpleoauth/build/.sass-cache'
---
+++ build/temp/production/SimpleOAuth/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/bryan/Projects/simpleoauth/app.js
