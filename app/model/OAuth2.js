618fa1063fb2abaa6b74e29e9f8bd3638d6bb621
Bryan P Johnson <bryan@12ftguru.com>
2015-05-21 13:28:49 -0400

    Fixes for some fields not saving.
    Signed-off-by: Bryan P Johnson <bryan@12ftguru.com>

--- app/model/OAuth2.js
+++ app/model/OAuth2.js
@@ -4,6 +4,7 @@ Ext.define('SimpleOAuth.model.OAuth2', {
     { name: 'client_secret', type: 'string' },
     { name: 'client_name', type: 'string' },
     { name: 'client_id', type: 'string' },
+    { name: 'client_user_id', type: 'string' },
     { name: 'user_id', type: 'string', defaultValue: 'FakeUserID' },
     { name: 'url_auth', type: 'string', defaultValue: 'http://127.0.0.1:8001/api/oauth2/authorize' },
     { name: 'url_token', type: 'string', defaultValue: 'http://127.0.0.1:8001/api/oauth2/token' },
